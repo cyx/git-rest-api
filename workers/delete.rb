5a94996bd765bb1de6f2729599ed2687c63db029
Cyril David <cyx@cyx.is>
2014-09-18 15:10:22 -0700

    Add quick hack in workers to rescue exceptions

--- workers/delete.rb
+++ workers/delete.rb
@@ -6,7 +6,10 @@ class DELETE
     $stderr.puts "       Repository.del(%s, %s, ...)" %
       [job.uri, job.path]
 
-    response = Repository.del(job.uri, job.path, job.payload)
+    # FIXME: Quick hack to make things work for now
+    status, response = Service.try do
+      Repository.del(job.uri, job.path, job.payload)
+    end
     $stderr.puts "       Got response: %s" % response
 
     job.done!(response)
--- workers/put.rb
+++ workers/put.rb
@@ -6,8 +6,12 @@ class PUT
     $stderr.puts "       Repository.put(%s, %s, ...)" %
       [job.uri, job.path]
 
-    response = Repository.put(job.uri, job.path, job.payload)
-    $stderr.puts "       Got response: %s" % response
+    # FIXME: Quick hack to make things work for now
+    status, response = Service.try do
+      Repository.put(job.uri, job.path, job.payload)
+    end
+
+    $stderr.puts "       Got response: %s, %s" % [status, response]
 
     job.done!(response)
     $stderr.puts "       Job done!"
