697d9013c094e7d991be0c46ca10f8f55c50f90a
Cyril David <cyx@cyx.is>
2014-09-18 12:19:52 -0700

    Add Heroku::account method
    Thanks to clint for the link to the docs :-)

--- lib/heroku.rb
+++ lib/heroku.rb
@@ -1,7 +1,34 @@
+require "requests"
+
 module Heroku
   GIT_URI = "https://:%{key}@git.heroku.com/%{app}.git"
+  API_URI = "https://api.heroku.com%s"
 
   def self.git_uri(app, key)
     GIT_URI % { app: app, key: key }
   end
+
+  def self.account(key)
+    account = cache(:account, key) do
+      resp = Requests.request("GET", API_URI % "/account",
+        auth: ["", key],
+        headers: { "Accept" => "application/vnd.heroku+json; version=3" })
+
+      resp.body
+    end
+
+    JSON.parse(account)
+
+  rescue JSON::ParserError, Requests::Error
+    return {}
+  end
+
+private
+  # FIXME : make this cache in redis / memcache
+  def self.cache(namespace, key)
+    key = "%s:%s" % [namespace, Digest::SHA1.hexdigest(key)]
+
+    @_cache[key] ||= yield
+  end
+  @_cache = {}
 end
