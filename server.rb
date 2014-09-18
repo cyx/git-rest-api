b0efbaf50f1e480ecb488b27cbf3cfb5fcfa91fa
Cyril David <cyx@cyx.is>
2014-09-18 11:13:10 -0700

    Add a rescue in basic_auth to return 403

--- server.rb
+++ server.rb
@@ -10,40 +10,49 @@ Cuba.plugin Basica
 Cuba.plugin JSONResponse
 
 Cuba.define do
-  basic_auth(env) do |_, api_key|
-    on ":app/repo" do |app|
-      uri = Heroku.git_uri(app, api_key)
+  begin
+    basic_auth(env) do |_, api_key|
+      on ":app/repo" do |app|
+        uri = Heroku.git_uri(app, api_key)
 
-      on /(.*)/ do |path|
-        on get do
-          status, resp = Service.get(uri, path)
-          json(status, resp)
-        end
+        on /(.*)/ do |path|
+          # Eliminate leading slashes for uniformity.
+          path = path.gsub(/^\//, '')
 
-        on put do
-          status, resp = Service.put(uri, path, req.params)
-          json(status, resp)
-        end
+          on get do
+            status, resp = Service.get(uri, path)
+            json(status, resp)
+          end
 
-        on post do
-          status, resp = Service.put(uri, path, req.params)
-          json(status, resp)
-        end
+          on put do
+            status, resp = Service.put(uri, path, req.params)
+            json(status, resp)
+          end
 
-        on delete do
-          status, resp = Service.delete(uri, path, req.params)
-          json(status, resp)
-        end
+          on post do
+            status, resp = Service.put(uri, path, req.params)
+            json(status, resp)
+          end
 
-        # This is a sort of joke from hipchat yesterday:
-        #
-        # Naaman Newbold: brownie points would be supporting PATCH
-        #
-        on req.patch? do
-          status, resp = Service.put(uri, path, req.params)
-          json(status, resp)
+          on delete do
+            status, resp = Service.delete(uri, path, req.params)
+            json(status, resp)
+          end
+
+          # This is a sort of joke from hipchat yesterday:
+          #
+          # Naaman Newbold: brownie points would be supporting PATCH
+          #
+          on req.patch? do
+            status, resp = Service.put(uri, path, req.params)
+            json(status, resp)
+          end
         end
       end
+    end
+  rescue RuntimeError
+    on default do
+      json(403, { error: 'Forbidden' })
     end
   end
 end
